024e3d305ef064b2f725757554d36fe8351d3e7a
Cody De Arkland <codydearkland@gmail.com>
2024-04-07 01:14:28 -0700

    removing speed insights, will fix later

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
       <Analytics />
-      <SpeedInsights />
     </>
   )
   
